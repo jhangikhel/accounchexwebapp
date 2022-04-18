7500aa4959c4c3cfe8b6e4ef4c9f6de8805a56bf
jhangikhel <gourav.jhangihel@gmail.com>
2022-04-18 16:34:19 +0530

    Resolve Issues

---
+++ src/Components/WorkBasket/WorkBasketContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+const WorkBasketContainer = () => {
+
+    return (
+        <div>
+           
+            sddsasddsdas
+        </div>
+    );
+};
+
+export default WorkBasketContainer;
--- src/pages/Workbasket.tsx
+++ src/pages/Workbasket.tsx
@@ -2,14 +2,17 @@
 import * as React from 'react';
 import WorkBasketTabs from './../Components/WorkBasket/WorkBasketTabs';
 import WorkbasketHeader from './../Components/WorkBasket/WorkbasketHeader';
+import WorkBasketContainer from './../Components/WorkBasket/WorkBasketContainer';
 type Props = {
     
 }; 
 const Workbasket = (props: Props) => {
     return (
         <>
+         
          <WorkbasketHeader />
             <WorkBasketTabs />
+            <WorkBasketContainer />
         </>
     );
 };
